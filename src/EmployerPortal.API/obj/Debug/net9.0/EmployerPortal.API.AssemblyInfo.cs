9fad4366cd4fc9212b47a337f68c68491d549a0a
Gustavo Matheus Schuster <schusterg.0401@gmail.com>
2025-05-26 22:03:39 -0300

    clean

--- src/EmployerPortal.API/obj/Debug/net9.0/EmployerPortal.API.AssemblyInfo.cs
+++ src/EmployerPortal.API/obj/Debug/net9.0/EmployerPortal.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployerPortal.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+097ae68c595893148e666d2f383da4fbdf456622")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployerPortal.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployerPortal.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
